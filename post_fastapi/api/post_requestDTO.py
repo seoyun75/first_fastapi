0bd30684a4af417cba77f56419c09de17a0842cb
seoyun75 <67564166+seoyun75@users.noreply.github.com>
2023-03-22 22:42:11 +0900

    refactor: 도메인객체에서 requestDTO 분리

--- post_fastapi/api/post_requestDTO.py
+++ post_fastapi/api/post_requestDTO.py
@@ -4,11 +4,15 @@ from typing import Optional
 from pydantic import BaseModel
 from sqlmodel import Field, SQLModel
 
+class PostUpdate(SQLModel):
+    user: Optional[str]
+    title: Optional[str]
+    content: Optional[str]
+    updated_at: datetime = Field(default_factory=datetime.today)
 
-class Post(SQLModel, table=True):
+
+class PostCreate(BaseModel):
     id: Optional[int] = Field(default=None, primary_key=True)
     user: str
     title: str
     content: str
-    created_at: datetime = Field(default_factory=datetime.today)
-    updated_at: Optional[datetime]
