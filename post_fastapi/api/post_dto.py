341b6f6686559ade0f37274cb56a32a87bfb3062
seoyun75 <67564166+seoyun75@users.noreply.github.com>
2023-06-18 05:01:03 +0900

    refactor: post dto 생성

---
+++ post_fastapi/api/post_dto.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from typing import Optional
+
+from sqlmodel import Field, SQLModel
+
+
+class PostUpdate(SQLModel):
+    user: Optional[str]
+    title: Optional[str]
+    content: Optional[str]
+    updated_at: datetime = Field(default_factory=datetime.today)
+
+
+class PostRequest(SQLModel):
+    user: Optional[str]
+    title: Optional[str]
+    content: Optional[str]
