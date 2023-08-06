52675f17d8a79965d2b4dad15e3fd9e916f9f74c
seoyun75 <67564166+seoyun75@users.noreply.github.com>
2023-08-06 21:33:57 +0900

    fix: comment 인가 구현

--- post_fastapi/domain/comment.py
+++ post_fastapi/domain/comment.py
@@ -6,7 +6,7 @@ from sqlmodel import Field, SQLModel
 
 class Comment(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    author_id: str
+    user_id: Optional[str]
     post_id: int
     content: str
     created_at: datetime = Field(default_factory=datetime.today)
