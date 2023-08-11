a7da17e31c8014a5d6b03c38971ebf0f22273a8d
seoyun75 <67564166+seoyun75@users.noreply.github.com>
2023-08-11 15:00:01 +0900

    fix: main.py 디버깅환경코드 추가

--- post_fastapi/main.py
+++ post_fastapi/main.py
@@ -1,4 +1,8 @@
-from api import auth, comment, post, user
+import uvicorn
+from api import auth
+from api.comment import comment_endpoint
+from api.post import post_endpoint
+from api.user import user_endpoint
 from click import UUID
 from db import init_db
 from fastapi import Depends, FastAPI
@@ -11,9 +15,10 @@ def on_startup():
     init_db()
 
 
-app.include_router(user.router, tags=["users"])
+app.include_router(user_endpoint.router, tags=["users"])
 app.include_router(auth.router, tags=["auth"])
-app.include_router(post.router, tags=["posts"])
-app.include_router(comment.router, tags=["comments"])
+app.include_router(post_endpoint.router, tags=["posts"])
+app.include_router(comment_endpoint.router, tags=["comments"])
 
-# @app.exception_handler(RequestValidationError)
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8080)
